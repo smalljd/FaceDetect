d58794cb5f17f8e49dd1525bc628490cdcbc5b37
Jeff Small <jeffreyd.small@gmail.com>
2019-01-30 21:14:26 -0800

    Initial Commit

---
+++ FaceDetect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetect
+//
+//  Created by Jeff Small on 1/30/19.
+//  Copyright © 2019 One Medical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FaceDetectTests/FaceDetectTests.swift
@@ -0,0 +1,34 @@
+//
+//  FaceDetectTests.swift
+//  FaceDetectTests
+//
+//  Created by Jeff Small on 1/30/19.
+//  Copyright © 2019 One Medical. All rights reserved.
+//
+
+import XCTest
+@testable import FaceDetect
+
+class FaceDetectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
